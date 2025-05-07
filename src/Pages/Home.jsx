97523b52bc0b6b2ddd44db9ada6ad6d695fc33f2
KookieMan <lucaspinheiro3ds@gmail.com>
2025-05-07 15:19:56 -0500

    Adicionar tag h1 em home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import { Buttons } from "../Components/Buttons";
 const Home = () => {
     return (
         <div>
+            <h1>teste</h1>
             <CounterFunction />
             <Buttons/>
         </div>
