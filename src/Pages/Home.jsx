a1813521a852809818227933cf9c974322cf0e9e
Pierrot <gustavobolseiro@outlook.com>
2025-05-07 15:46:56 -0500

    Criando nova branch e coocando um h1 diferente

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import { Buttons } from "../Components/Buttons";
 const Home = () => {
     return (
         <div>
+            <h1>ESSA É A BRANCH DE GUSTAVO!</h1>
             <CounterFunction />
             <Buttons/>
         </div>
